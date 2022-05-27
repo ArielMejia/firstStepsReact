3a7a74321fee7748b47d7047dcb387d61cb1b1fe
ArielMejia <kevinmejia_2010@hotmail.com>
2022-05-26 21:59:42 -0600

    Add header in all pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Header from '../components/Header'
+
 
 const Home = () => {
     return (
-        <Header />
+        <h1>Welcome to Home</h1>
     );
 };
 
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import ChangePassword from '../containers/ChangePassword';
+import ChangePassword from '../pages/ChangePassword';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
+import Login from '../pages/Login';
+import RecoveryPassword from '../pages/RecoveryPassword';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css'
